0f4fd4f7b233efb8abd74c7072c04d77d869a285
radiomiu <radiomiu@gmail.com>
2013-09-16 00:12:57 +0400

    Update yapi.py

--- yapi.py
+++ yapi.py
@@ -3,17 +3,21 @@ import json, urllib2
 class YandexApiService(object):
     sandbox_url = 'https://api-sandbox.direct.yandex.ru/json-api/v4/'
     
-    def __init__(self, token): 
+    def __init__(self, token):
         self.token = token
         
     def query(self, method, params=None):
-        query_params = {'token': self.token, 'method': method} 
-        if params: query_params['param'] = params 
+        query_params = {'token': self.token, 'method': method}
+        if params: query_params['param'] = params
+        
         jdata = json.dumps(query_params, ensure_ascii=False).encode('utf8')
         response = urllib2.urlopen(self.sandbox_url, jdata).read()
-        decoded_responce = json.loads(response)
+        return self.__handle_response(response)
+            
+    def __handle_response(self, response):
         try:
-            return decoded_responce["data"]
-        except:
-            print decoded_responce
-            raise
+            decoded_responce = json.loads(response)
+            return decoded_responce['data']
+        except KeyError as detail:
+            print 'Error fetching data from ', response
+            raise detail
