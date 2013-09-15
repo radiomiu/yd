62d34064b6b4fa698210c7a7fa587589a3d49e9a
radiomiu <radiomiu@gmail.com>
2013-09-16 00:13:34 +0400

    Update stopbanners.py

--- stopbanners.py
+++ stopbanners.py
@@ -14,14 +14,15 @@ for campaign in activecampaigns:
     if campaign_id % 2 == 0:
         grouped_odd_banners[campaign_id] = []
         even_campaign_ids.append(campaign_id)
-        
-banners = client.query('GetBanners', {'CampaignIDS': even_campaign_ids})
  
-for banner in banners:
-    banner_id = banner['BannerID']
-    if banner_id % 2 != 0:
-        grouped_odd_banners[banner['CampaignID']].append(banner_id)
- 
-for campaign_id, odd_banner_ids in grouped_odd_banners.iteritems():
-    client.query('ResumeBanners', {'CampaignID': campaign_id, 'BannerIDS': odd_banner_ids})
-    print odd_banner_ids
+if even_campaign_ids:       
+    banners = client.query('GetBanners', {'CampaignIDS': even_campaign_ids})
+     
+    for banner in banners:
+        banner_id = banner['BannerID']
+        if banner_id % 2 != 0:
+            grouped_odd_banners[banner['CampaignID']].append(banner_id)
+     
+    for campaign_id, odd_banner_ids in grouped_odd_banners.iteritems():
+        client.query('ResumeBanners', {'CampaignID': campaign_id, 'BannerIDS': odd_banner_ids})
+        print odd_banner_ids
