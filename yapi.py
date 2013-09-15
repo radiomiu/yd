47170e1626f262e3792d6b945445acedfbfba382
radiomiu <radiomiu@gmail.com>
2013-09-15 21:32:41 +0400

    Create yapi.py

---
+++ yapi.py
@@ -0,0 +1,16 @@
+import json, urllib2
+ 
+class YandexApiService(object):
+    sandbox_url = 'https://api-sandbox.direct.yandex.ru/json-api/v4/'
+    
+    def __init__(self, token): 
+        self.token = token
+        
+    def query(self, method, params=None):
+        query_params = {'token': self.token, 'method': method} 
+        if params: query_params['param'] = params 
+        
+        jdata = json.dumps(query_params, ensure_ascii=False).encode('utf8') 
+        response = urllib2.urlopen(self.sandbox_url, jdata).read()
+        decoded_responce = json.loads(response)
+        return decoded_responce["data"]
